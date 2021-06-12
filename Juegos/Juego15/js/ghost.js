5c4448256883cd7b6b8b210587ed142c7458540c
Vector1338 <computerfix.computerfix@gmail.com>
2021-06-12 01:20:55 -0400

    8

--- Juegos/Juego15/js/ghost.js
+++ Juegos/Juego15/js/ghost.js
@@ -66,7 +66,7 @@ ghosts.forEach(g => {
             g.h=g.h-1;
             g.h = g.h==-1 ? 18 : g.h;
         }
-        g.elementAddColor(document.querySelectorAll("#board>div")[(g.v*19)+g.h]);
+        g.elementAddColor(document.querySelectorAll("#board>div")[(g.v*30)+g.h]);
 
         // revisamos si hay colision con pacman
         checkCollision(g);
