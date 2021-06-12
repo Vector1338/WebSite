5bd94a012183de5d7be339aee301c5c2fef75057
Vector1338 <computerfix.computerfix@gmail.com>
2021-06-12 01:23:30 -0400

    {ñ

--- Juegos/Juego15/js/ghost.js
+++ Juegos/Juego15/js/ghost.js
@@ -56,17 +56,17 @@ ghosts.forEach(g => {
         g.direction=newDirection(g);
         g.elementRemoveColor(document.querySelectorAll("#board>div")[(g.v*19)+g.h]);
         if (g.direction==0 && gCanUp(g)) {
-            g.v=g.v-1;
+            g.v=g.v-0.9;
         } else if (g.direction==1 && gCanRight(g)) {
-            g.h=g.h+1;
+            g.h=g.h+0.9;
             g.h = g.h==19 ? 0 : g.h;
         } else if (g.direction==2 && gCanDown(g)) {
-            g.v=g.v+1;
+            g.v=g.v+0.9;
         } else if (g.direction==3 && gCanLeft(g)) {
-            g.h=g.h-1;
+            g.h=g.h-0.9;
             g.h = g.h==-1 ? 18 : g.h;
         }
-        g.elementAddColor(document.querySelectorAll("#board>div")[(g.v*12)+g.h]);
+        g.elementAddColor(document.querySelectorAll("#board>div")[(g.v*19)+g.h]);
 
         // revisamos si hay colision con pacman
         checkCollision(g);
